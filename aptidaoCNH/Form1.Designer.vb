c0c67be0af7ccd53235cf15868d59af80ad3bb56
Sheila Kerber <sheila.skerber@gmail.com>
2021-08-29 15:11:50 -0300

    Cria aplicação

---
+++ aptidaoCNH/Form1.Designer.vb
@@ -0,0 +1,99 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblNome = New System.Windows.Forms.Label()
+        Me.txtNome = New System.Windows.Forms.TextBox()
+        Me.lblNascimento = New System.Windows.Forms.Label()
+        Me.datePicker = New System.Windows.Forms.DateTimePicker()
+        Me.btnVerificar = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblNome
+        '
+        Me.lblNome.AutoSize = True
+        Me.lblNome.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblNome.Location = New System.Drawing.Point(54, 52)
+        Me.lblNome.Name = "lblNome"
+        Me.lblNome.Size = New System.Drawing.Size(48, 16)
+        Me.lblNome.TabIndex = 0
+        Me.lblNome.Text = "Nome:"
+        '
+        'txtNome
+        '
+        Me.txtNome.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtNome.Location = New System.Drawing.Point(57, 71)
+        Me.txtNome.Name = "txtNome"
+        Me.txtNome.Size = New System.Drawing.Size(200, 22)
+        Me.txtNome.TabIndex = 1
+        '
+        'lblNascimento
+        '
+        Me.lblNascimento.AutoSize = True
+        Me.lblNascimento.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblNascimento.Location = New System.Drawing.Point(54, 106)
+        Me.lblNascimento.Name = "lblNascimento"
+        Me.lblNascimento.Size = New System.Drawing.Size(83, 16)
+        Me.lblNascimento.TabIndex = 2
+        Me.lblNascimento.Text = "Nascimento:"
+        '
+        'datePicker
+        '
+        Me.datePicker.Location = New System.Drawing.Point(57, 125)
+        Me.datePicker.Name = "datePicker"
+        Me.datePicker.Size = New System.Drawing.Size(200, 20)
+        Me.datePicker.TabIndex = 3
+        '
+        'btnVerificar
+        '
+        Me.btnVerificar.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnVerificar.Location = New System.Drawing.Point(50, 186)
+        Me.btnVerificar.Name = "btnVerificar"
+        Me.btnVerificar.Size = New System.Drawing.Size(207, 27)
+        Me.btnVerificar.TabIndex = 4
+        Me.btnVerificar.Text = "Verificar"
+        Me.btnVerificar.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(307, 288)
+        Me.Controls.Add(Me.btnVerificar)
+        Me.Controls.Add(Me.datePicker)
+        Me.Controls.Add(Me.lblNascimento)
+        Me.Controls.Add(Me.txtNome)
+        Me.Controls.Add(Me.lblNome)
+        Me.Name = "Form1"
+        Me.Text = "Teste de aptidão para habilitação"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblNome As System.Windows.Forms.Label
+    Friend WithEvents txtNome As System.Windows.Forms.TextBox
+    Friend WithEvents lblNascimento As System.Windows.Forms.Label
+    Friend WithEvents datePicker As System.Windows.Forms.DateTimePicker
+    Friend WithEvents btnVerificar As System.Windows.Forms.Button
+
+End Class
---
+++ aptidaoCNH/Form1.vb
@@ -0,0 +1,35 @@
+﻿Public Class Form1
+    ' configurando o formato da data para dia-mes-ano
+    Private Sub datePicker_ValueChanged(sender As Object, e As EventArgs) Handles datePicker.ValueChanged
+        datePicker.Format = DateTimePickerFormat.Custom
+        datePicker.CustomFormat = "dd-MM-yyyy"
+    End Sub
+
+    ' ao clicar no botao 'Verificar'
+    Private Sub btnVerificar_Click(sender As Object, e As EventArgs) Handles btnVerificar.Click
+        ' declaracao de variaveis
+        Dim nascimento, dataAtual As Date
+        Dim idade As Integer
+        Dim nome As String
+
+        ' validacao do input nome
+        If (txtNome.Text = Nothing Or IsNumeric(txtNome.Text)) Then
+            MsgBox("O campo nome não está preenchido ou não é válido!", MsgBoxStyle.Critical, "Atenção!")
+            Exit Sub
+        End If
+
+        nome = txtNome.Text
+        nascimento = datePicker.Value
+        dataAtual = Now     'data atual do sistema
+
+        ' calculo da idade (/365.25 para considerar anos bissextos)
+        idade = DateDiff(DateInterval.Day, nascimento, dataAtual) / 365.25
+
+        ' lógica para exibir o resultado
+        If (idade >= 18) Then
+            MsgBox(txtNome.Text & " tem " & Format(idade, "##") & " anos e, portanto, ESTÁ apto(a) para fazer a habilitação.", MsgBoxStyle.Information, "Resultado:")
+        Else
+            MsgBox(txtNome.Text & " tem " & Format(idade, "##") & " ano(s) e, portanto, NÃO ESTÁ apto(a) para fazer a habilitação.", MsgBoxStyle.Information, "Resultado:")
+        End If
+    End Sub
+End Class
